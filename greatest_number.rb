5d68e06edefe1edbbf26967889eccdf545d33c72
kaizo <ushakya3@gmail.com>
2023-03-28 13:42:54 +0545

    largest function refactor

--- greatest_number.rb
+++ greatest_number.rb
@@ -1,15 +1,27 @@
 # Given three numbers X, Y & Z. write a function/method that finds the greatest among the numbers.
-numbers = []
+# numbers = []
 
-def show_greatest(numbers)
-  greatest = numbers.max(1) # returns single maximum value
-  puts "Greatest number among three is #{greatest.first}"
+# def show_greatest(numbers)
+#   greatest = numbers.max(1) # returns single maximum value
+#   puts "Greatest number among three is #{greatest.first}"
+# end
+
+# 3.times do
+#   puts "Enter number to compare :"
+#   number = gets.chomp.to_f
+#   numbers.push(number)
+# end
+
+# show_greatest(numbers)
+
+a = 1 
+b = 1.5
+c = 3
+
+def largest(a,b,c)
+  return a if a > b && a > c
+  return b if b > a && b > c
+  return c if c > a && c > b
 end
 
-3.times do
-  puts "Enter number to compare :"
-  number = gets.chomp.to_f
-  numbers.push(number)
-end
-
-show_greatest(numbers)
+puts largest(a,b,c)
