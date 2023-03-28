db3607ea376581aa7bc33adc6e19c8eda1393de1
kaizo <ushakya3@gmail.com>
2023-03-28 13:56:43 +0545

    replace ruby method with if

--- count_occurence.rb
+++ count_occurence.rb
@@ -5,12 +5,19 @@
 #     Output: h: 2, e: 2, o: 3, l: 2, w:1, r:1, y: 1
 puts "Enter a sentence to count occurence of its characters:"
 sentence = gets.chomp.to_s
-unique_chars = sentence.split("").uniq
-unique_chars.delete(" ") if unique_chars.include?(" ")
+characters = sentence.split("")
+characters.delete(" ") if characters.include?(" ")
+unique_chars = characters.uniq
 counts = Hash.new
-unique_chars.each do |char|
-  count = sentence.count(char)
-  counts[char]=count
+unique_chars.each do |count_letter|
+  count = 0
+  characters.each do |char|
+    if count_letter == char
+      count = count + 1 
+    end
+  # count = sentence.count(char)
+  counts[count_letter]=count
+end
 end
 counts.each do |key,value| 
   puts "#{key} : #{value}"
